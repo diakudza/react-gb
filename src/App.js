897b14ceafb5c2c9138c5cf03306eb5f31db5466
diakudza <diakudza@gmail.com>
2021-07-21 22:03:23 +0300

    исправления 2

--- src/App.js
+++ src/App.js
@@ -8,27 +8,32 @@ let App = () => {
 
     const [messages, setMessages] = useState([{author: "ROBOT", text: "Привет, Миха", ai: true}]);
 
-    let messagesArr = messages.map((el,index) => <Message key={index} state={el}/>)
+    let messagesArr = messages.map((el, index) => <Message key={index} state={el}/>)
 
     let setMessage = (message) => {
-        if (message !== "") {
-            setMessages([...messages, {author: "mixa", text: message, ai: false}])
-        } else {
-            setTimeout(() => {
-                setMessages([...messages, {author: "ROBOT", text: `Зачем отправляешь пустоту?`, ai: true}])
-            }, (Math.random() * 2) * 1000)
-        }
+        setMessages([...messages, {author: "mixa", text: message, ai: false}])
+    }
+
+    let sendMessageFromRobot = (message) => {
+        setTimeout(() => {
+            setMessages([...messages, {author: "ROBOT", text: message, ai: true}])
+        }, (Math.random() * 2) * 1000)
     }
 
     useEffect(() => {
-        if (messages[messages.length - 1].author !== 'ROBOT' ) {
+        if (messages[messages.length - 1].author !== 'ROBOT') {
             let lastMessage = messages[messages.length - 1].text
-            setTimeout(() => {
-                setMessages([...messages, {author: "ROBOT", text: ` Я не пойму "${lastMessage}" , что ты имеешь в виду?`, ai: true}])
-            }, (Math.random() * 2) * 1000)
+            sendMessageFromRobot(`Я не пойму "${lastMessage}" ,что ты имеешь в виду?`)
         }
     }, [messages])
 
+    useEffect(() => {
+        if (messages[messages.length - 1].text == '') {
+            messages.pop()
+            setMessages(messages)
+            sendMessageFromRobot(`А зачем пустоту слать?`)
+        }
+    }, [messages[messages.length - 1].text])
 
     return (
 
